d80784c2ef628c406c19fa32c959db22d4774237
Deborah Barcenas <deborah.barcenas@gmail.com>
2020-06-15 20:03:59 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
     #   in session[:id]
     # set that user equal to a variable, @user, so that the view found in
     #   app/views/users/home.erb can render that user
-    # @user = User.find(session[:id])
-    @user = User.find(session[:user_id])
+    @user = User.find(session[:id])
+    # @user = User.find(session[:user_id])
     erb :'/users/home'
   end
 
