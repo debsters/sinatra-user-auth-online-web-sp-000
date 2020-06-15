b85e5efca09804d45dbc4360d2b2ff8fcac6d98e
Deborah Barcenas <deborah.barcenas@gmail.com>
2020-06-15 19:48:36 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params["email"], password: params["password"])
     if @user
        session[:user_id] = @user.id
-      session[:id] = @user.id
+      # session[:id] = @user.id
       redirect '/users/home'
     end
     redirect '/sessions/login'
